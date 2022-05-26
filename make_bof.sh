83f538c29f0f63be977fb80486a2bb1679a10800
Cory Cardio <ccardio@bishopfox.com>
2022-05-26 08:53:20 -0400

    add Psw and Winver

--- make_bof.sh
+++ make_bof.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
 
 HOME=$(pwd)
+BOF=$1
 
-cd ./BOF/Kerberoast/SOURCE
+echo "[+] Changing directory: ./BOF/$BOF/SOURCE"
+cd ./BOF/$BOF/SOURCE
+echo "[+] Compiling:"
 make
-cd .. # ./BOF/Kerberoast/
+cd .. # ./BOF/$BOF/
+
+echo "[+] Creating artifact:"
 mkdir artifacts
 mv *.o ./artifacts/
-echo
-pwd
-ls -l
-echo
 VERSION=$(git describe --tags --abbrev=0)
 cat extension.json | jq ".version |= \"$VERSION\"" > ./artifacts/extension.json
-cd artifacts # ./BOF/Kerberoast/artifacts/
+cd artifacts # ./BOF/$BOF/artifacts/
 echo
 pwd
 ls -l
 echo
 
+echo "[+] Creating package:"
 MANIFEST=$(cat extension.json | base64 -w 0)
 COMMAND_NAME=$(cat extension.json | jq -r .command_name)
 echo "[+] executing: tar -czvf $HOME/packages/$COMMAND_NAME.tar.gz ."
 tar -czvf $HOME/packages/$COMMAND_NAME.tar.gz .
-cd $HOME
+cd $HOME/packages
 echo
 pwd
 ls -l
-ls -l ../
-cd ./packages
-echo
-pwd
-ls -l
-echo "[+] Listing ~/"
-ls -l ~/
+
+echo "[+] Signing package:"
 #bash -c "echo \"\" | ~/minisign -s ~/minisign.key -S -m ./$COMMAND_NAME.tar.gz -t \"$MANIFEST\" -x $COMMAND_NAME.minisig"
-echo "[+] Listing /home/runner/"
-ls -l /home/runner/
-echo "[+] Signing package"
 bash -c "echo \"\" | /home/runner/minisign -s /home/runner/minisign.key -S -m ./$COMMAND_NAME.tar.gz -t \"$MANIFEST\" -x $COMMAND_NAME.minisig"
