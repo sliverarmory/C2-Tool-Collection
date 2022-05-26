8cab88ce0748410967ba8ab79da3298169dc21f2
Cory Cardio <ccardio@bishopfox.com>
2022-05-26 06:55:38 -0400

    cont. debug of make_bof

--- make_bof.sh
+++ make_bof.sh
@@ -21,12 +21,13 @@ echo
 
 MANIFEST=$(cat extension.json | base64 -w 0)
 COMMAND_NAME=$(cat extension.json | jq -r .command_name)
-echo "[+] executing: tar -czvf $HOME/$COMMAND_NAME.tar.gz ."
-tar -czvf $HOME/$COMMAND_NAME.tar.gz .
+echo "[+] executing: tar -czvf $HOME/packages/$COMMAND_NAME.tar.gz ."
+tar -czvf $HOME/packages/$COMMAND_NAME.tar.gz .
 cd $HOME
 echo
 pwd
 ls -l
+ls -l ../
 cd ./packages
 echo
 pwd
